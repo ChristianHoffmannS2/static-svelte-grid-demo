8832689bf548d3ed1b47de79f4f371b0b3f2579d
christian.b.hoffmann <christian.b.hoffmann@accenture.com>
2023-01-14 17:46:37 +0100

    base page copied

--- src/routes/Base/index.svelte
+++ src/routes/Base/index.svelte
@@ -1,3 +1,12 @@
-<div>
-	<h2>Base</h2>
-</div>
+<script lang="ts">
+	import { Link } from "svelte-navigator";
+	const welcomeString: string = 'Welcome to the svelte/smui/ag-grid demo!';
+</script>
+
+<h1>{welcomeString}</h1>
+<p>Visit <a href="https://svelte.dev/docs">svelte.dev</a> to read the svelte language documentation</p>
+<p>Visit <a href="https://sveltematerialui.com">sveltematerialui.com</a> to read the svelte material ui documentation</p>
+<p>Visit <a href="https://www.ag-grid.com/javascript-data-grid/grid-api/">ag-grid.com</a> to read the ag-grid documentation</p>
+
+<hr>
+<p>Jump to the <Link to="/grid">grid</Link>!</p>

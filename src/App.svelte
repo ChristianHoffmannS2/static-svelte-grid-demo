0cd187cc98943084c29e990c05eccdcb716d8ac2
christian.b.hoffmann <christian.b.hoffmann@accenture.com>
2023-01-14 13:15:24 +0100

    inital esbuild build

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Index from "./app/index.svelte";
+</script>
+
+<style>
+  .test {
+    display: inline-block;
+    background-color: #ff3e00;
+    color: white;
+  }
+
+  .secondStyle {
+    display: block;
+  }
+</style>
+
+<div class="test">
+  <label for="testingInput">This gets changed when you press the button:
+  </label>
+  <input type="text" name="testingInput" />
+</div>
+
+<div class="secondStyle">
+  <Index />
+</div>
